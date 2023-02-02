264e74b27e63248df7f81a61cbad4e05acf6d5bd
duonglq-itsj <duonglq-itsj@hikesiya.co.jp>
2023-02-02 16:11:53 +0700

    update

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ const c = canvas.getContext('2d');
 
 canvas.width = 1280;
 canvas.height = 768;
-
 c.fillStyle = 'white';
 c.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -39,34 +38,69 @@ image.src = './img/gameMap.png';
 
 const enemies = [];
 
-for (let i = 1; i < 10; i++) {
-    const xOffset = i * 150;
-    enemies.push(new Enemy({
-        position: {
-            x: waypoints[0].x - xOffset,
-            y: waypoints[0].y
-        }
-    }));
+function spawnEnemies(spawnCount) {
+    for (let i = 1; i < spawnCount + 1; i++) {
+        const xOffset = i * 150;
+        enemies.push(new Enemy({
+            position: {
+                x: waypoints[0].x - xOffset,
+                y: waypoints[0].y
+            }
+        }));
 
+    }
 }
 
 const buildings = [];
 let activeTile = undefined;
+let enemyCount = 3;
+let hearts = 10;
+let coins = 100;
+const explosions = [];
+spawnEnemies(enemyCount);
 
 function animate() {
-    requestAnimationFrame(animate);
+    const animationId = requestAnimationFrame(animate);
     c.drawImage(image, 0, 0);
 
-    enemies.forEach(enemy => {
+    for (let i = enemies.length - 1; i >= 0; i--) {
+        const enemy = enemies[i];
         enemy.update();
-    });
+        if (enemy.position.x > canvas.width) {
+            hearts -= 1;
+            enemies.splice(i, 1);
+            document.querySelector('#hearts').innerHTML = hearts;
+
+            if (hearts === 0) {
+                console.log('Game over');
+                cancelAnimationFrame(animationId);
+                document.querySelector('#gameOver').style.display = 'flex';
+            }
+        }
+    }
+
+    for (let i = explosions.length - 1; i >= 0; i--) {
+        const explosion = explosions[i];
+        explosion.draw();
+        explosion.update();
+
+        if (explosion.frames.current >= explosion.frames.max - 1) {
+            explosions.splice(i, 1);
+        }
+    }
+
+    // tracking total amount of enemies
+    if (enemies.length === 0) {
+        enemyCount += 2;
+        spawnEnemies(enemyCount);
+    }
 
     placementTiles.forEach((tile) => {
         tile.update(mouse);
     });
 
     buildings.forEach((building) => {
-        building.draw();
+        building.update();
         building.target = null;
         const validEnemies = enemies.filter(enemy => {
             const xDifference = enemy.center.x - building.center.x;
@@ -83,7 +117,27 @@ function animate() {
             const xDifference = projectile.enemy.center.x - projectile.position.x;
             const yDifference = projectile.enemy.center.y - projectile.position.y;
             const distance = Math.hypot(xDifference, yDifference);
+
+            // this is when a projectile hits an enemy
             if (distance < projectile.enemy.radius + projectile.radius) {
+                // enemy health and enemy removal
+                projectile.enemy.health -= 20;
+                if (projectile.enemy.health <= 0) {
+                    const enemyIndex = enemies.findIndex((enemy) => {
+                        return projectile.enemy === enemy;
+                    });
+                    if (enemyIndex > -1) {
+                        enemies.splice(enemyIndex, 1);
+                        coins += 25;
+                        document.querySelector('#coins').innerHTML = coins;
+                    };
+                }
+                explosions.push(new Sprite({
+                    position: { x: projectile.position.x, y: projectile.position.y },
+                    imageSrc: './img/explosion.png',
+                    frames: { max: 4 },
+                    offset: { x: 0, y: 0 }
+                }));
                 building.projectiles.splice(i, 1);
             }
         }
@@ -96,7 +150,9 @@ const mouse = {
 }
 
 canvas.addEventListener('click', (event) => {
-    if (activeTile && !activeTile.isOccupied) {
+    if (activeTile && !activeTile.isOccupied && coins - 50 >= 0) {
+        coins -= 50;
+        document.querySelector('#coins').innerHTML = coins;
         buildings.push(new Building({
             position: {
                 x: activeTile.position.x,
@@ -104,6 +160,9 @@ canvas.addEventListener('click', (event) => {
             }
         }));
         activeTile.isOccupied = true;
+        buildings.sort((a, b) => {
+            return a.position.y - b.position.y
+        });
     }
 });
 
